c3e28f4d0e53b840f72764d48eeeca29fd5ef4c6
Matthew Davis <matthew@matthewdavis.io>
2018-10-23 22:58:51 -0400

    added probes

--- src/main/java/com/example/k8byexamplesspringrest/TestRestController.java
+++ src/main/java/com/example/k8byexamplesspringrest/TestRestController.java
@@ -2,14 +2,25 @@ package com.example.k8byexamplesspringrest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 public class TestRestController {
+
+    @GetMapping("/is_ready")
+    public ResponseEntity<String> isReady() {
+
+        return new ResponseEntity<>("I'm ready!", HttpStatus.OK);
+
+    }
+
+    @GetMapping("/is_alive")
+    public ResponseEntity<String> isAlive() {
+
+        return new ResponseEntity<>("I'm alive!", HttpStatus.OK);
+
+    }
 
     @PostMapping("/echo")
     public ResponseEntity<?> testResponse(@RequestBody String body) {
